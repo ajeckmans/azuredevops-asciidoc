2562c6d05e50e502dac573813547eb8a52b45a47
ajeckmans <2088777+ajeckmans@users.noreply.github.com>
2026-06-18 13:29:17 +0200

    Configure Jest to mock CSS imports

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['**/__tests__/**/*.test.ts?(x)'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
+  },
 };
